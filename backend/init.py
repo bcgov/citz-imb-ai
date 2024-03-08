1784fee01953e54212ac6685dbe0d963b9efec8a
ANTSAND <antsand2002@gmail.com>
2024-03-07 21:50:23 -0800

    Vectorized DB + Jupyter interface

--- backend/init.py
+++ backend/init.py
@@ -15,7 +15,7 @@ from langchain_community.document_loaders import TextLoader
 from langchain_community.vectorstores.pgvector import PGVector
 from langchain_text_splitters import CharacterTextSplitter
 
-time.sleep(10)
+#time.sleep(10)
 
 ##################### DATABASE CONNECTIONS ############################
 
@@ -27,27 +27,66 @@ POSTGRES_HOST = os.getenv('POSTGRES_HOST')
 
 sqlversion = sqlalchemy.__version__
 
-connection_string = f'postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}'
+CONNECTION_STRING = f'postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}'
 
 print(sqlversion)
-#engine = create_engine("postgresql+psycopg://postgres:root@postgres:5432/ai_db")
-
-engine = create_engine(connection_string)
-
-with engine.connect() as conn:
-    conn.execute(text('CREATE EXTENSION IF NOT EXISTS vector'))
-    conn.commit()
-
-Base = declarative_base()
 
 ########################################################################
 
-##################### INITIALIZE EMBEDDINGS ############################
+##################### EXAMPLE EMBEDDINGS ############################
 embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 text = "This is a test document."
 query_result = embeddings.embed_query(text)
 doc_result = embeddings.embed_documents([text, "This is not a test document."])
-print(doc_result)
+
+##################### INITIALIZE EMBEDDINGS ############################
 loader = TextLoader("DB/RawBCLaws/all_act_titles.txt")
+documents = loader.load()
+text_splitter = CharacterTextSplitter(chunk_size=1, chunk_overlap=0, separator="\n")
+docs = text_splitter.split_documents(documents)
+
+COLLECTION_NAME = "bc_law_titles"
+
+# If the database table already exists, delete it
+db = PGVector.from_documents(
+    documents=docs,
+    embedding=embeddings,
+    collection_name=COLLECTION_NAME,
+    connection_string=CONNECTION_STRING,
+    pre_delete_collection=True,
+)
+
+##################### Test the similarity search ###############################
+query = "Is there a law on tenancy act?"
+print('\n' + query + '\n')
+docs_with_score = db.similarity_search_with_score(query)
+
+for doc, score in docs_with_score:
+    print("-" * 80)
+    print("Score: ", score)
+    print(doc.page_content)
+    print("-" * 80)
+
+#########################################################################
+
+##################### INITIALIZE DOCSTORE ###############################
+# If you want to connect to an existing collection  and test 
+# the similarity search   
+store = PGVector(
+    collection_name=COLLECTION_NAME,
+    connection_string=CONNECTION_STRING,
+    embedding_function=embeddings,
+) 
+
+query = "Is there any laws for landlords?"
+print('\n' + query + '\n')
+docs_with_score = store.similarity_search_with_score(query)
+
+for doc, score in docs_with_score:
+    print("-" * 80)
+    print("Score: ", score)
+    print(doc.page_content)
+    print("-" * 80)
+
 
 #########################################################################
