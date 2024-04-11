dfb0078442f3a0fa4bf264438d2d9e4c26af76e2
ANTSAND <antsand2002@gmail.com>
2024-04-11 09:04:02 -0700

    Add S3 pagination script for regulation

---
+++ backend/s3_Reg.py
@@ -0,0 +1,34 @@
+import boto3
+import os
+
+S3_ACCESS_KEY = os.getenv('S3_ACCESS_KEY')
+S3_SECRET_ACCESS_KEY = os.getenv('S3_SECRET_ACCESS_KEY')
+S3_ENDPOINT_URL = os.getenv('S3_ENDPOINT_URL')
+
+linode_obj_config = {
+    "aws_access_key_id": S3_ACCESS_KEY,
+    "aws_secret_access_key": S3_SECRET_ACCESS_KEY,
+    "endpoint_url": S3_ENDPOINT_URL,
+}
+bucket_name = "IMBAIPilot"
+
+prefix = "bclaws/data/xml/Cleaned/Regulations"
+
+client = boto3.client("s3", **linode_obj_config)
+
+paginator = client.get_paginator('list_objects_v2')
+pages = paginator.paginate(Bucket=bucket_name, Prefix=prefix)
+BASE_PATH = "XMLReg/"
+count = 0
+
+if not os.path.exists(BASE_PATH): os.makedirs(BASE_PATH, mode=0o777)
+
+for page in pages:
+    for obj in page['Contents']:
+        newpath = os.path.join(BASE_PATH, obj['Key'].split('/')[-1])
+        count +=1
+        print(f"doucument # {count}")
+        if newpath != "XMLReg/":
+            print(newpath)
+            client.download_file(bucket_name, obj['Key'], newpath)
+
