dbc117f1afbe7e8fed5a0186362140a7f3a170d5
Noor Chasib <noorchasib@gmail.com>
2024-05-22 12:17:33 -0700

    update sidebar styling and add icons

--- web/frontend/src/pages/Home/Sidebar/Sidebar.tsx
+++ web/frontend/src/pages/Home/Sidebar/Sidebar.tsx
@@ -4,15 +4,15 @@ import { assets } from '@/assets/icons/assets';
 import './Sidebar.scss';
 
 const Sidebar = () => {
-  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(true);
 
   const toggleSidebar = () => {
     setIsCollapsed(!isCollapsed);
   };
 
   return (
-    <div className={`sidebar ${isCollapsed ? 'collapsed' : ''}`}>
-      <div className="sidebar-header">
+    <div className={`sidebar ${isCollapsed ? 'collapsed' : 'expanded'}`}>
+      <div className="sidebar-header" title="Menu">
         <img
           src={assets.menu_icon}
           className="menu-icon"
@@ -20,17 +20,16 @@ const Sidebar = () => {
           onClick={toggleSidebar}
         />
       </div>
-      <ul>
-        <li>
-          <p>Section 1</p>
-        </li>
-        <li>
-          <p>Section 2</p>
-        </li>
-        <li>
-          <p>Section 3</p>
-        </li>
-      </ul>
+
+      <div className="new-chat" title="New Chat">
+        <img src={assets.plus_icon} alt="new chat" />
+        {!isCollapsed ? <p>New Chat</p> : null}
+      </div>
+
+      <div className="bottom" title="Logout">
+        <img src={assets.user_icon} alt="user" />
+        {!isCollapsed ? <p>Logout</p> : null}
+      </div>
     </div>
   );
 };
