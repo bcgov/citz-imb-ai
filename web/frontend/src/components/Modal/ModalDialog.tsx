b51da5f3ffd97a44bc0c9d67c85b00a3fcdc5050
Noor Chasib <noorchasib@gmail.com>
2024-05-29 15:08:32 -0700

    create modal dialog

---
+++ web/frontend/src/components/Modal/ModalDialog.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './ModalDialog.scss';
+
+interface ModalDialogProps {
+  title: string;
+  description: React.ReactNode;
+  option1: {
+    text: string;
+    onClick: () => void;
+  };
+  option2: {
+    text: string;
+    onClick: () => void;
+  };
+}
+
+const ModalDialog: React.FC<ModalDialogProps> = ({ title, description, option1, option2 }) => {
+  return (
+    <div className="modal-overlay">
+      <div className="modal-dialog">
+        <div className="modal-header">
+          <p className="modal-title">{title}</p>
+        </div>
+        <div className="modal-body">
+          <p className="modal-description">{description}</p>
+        </div>
+        <div className="modal-footer">
+          <button className="modal-button blue-button" onClick={option1.onClick}>
+            {option1.text}
+          </button>
+          <button className="modal-button red-button" onClick={option2.onClick}>
+            {option2.text}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ModalDialog;
