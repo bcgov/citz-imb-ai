2f08877b10a891a866fc3b47d8bad71b348d7aa6
Shivakumar <Anthony.Shivakumar@gov.bc.ca>
2024-05-29 08:59:31 -0700

    Update entryfile dos2x and using trulens postgres db

--- web/backend-fastapi/entrypoint.sh
+++ web/backend-fastapi/entrypoint.sh
@@ -3,7 +3,8 @@
 # Load environment variables
 echo "Loading environment variables..."
 
-export AWS_ACCESS_KEY_ID=""
+export AWS_ACCESS_KEY_ID="A"
+
 
 
 # Check if the environment variables are loaded correctly
