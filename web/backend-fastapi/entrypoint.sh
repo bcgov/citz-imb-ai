824f117ea2bbbc6d562d0ec374c24ab2689ed038
Shivakumar <Anthony.Shivakumar@gov.bc.ca>
2024-06-04 12:17:18 -0700

    Update refresh token handling in backend

--- web/backend-fastapi/entrypoint.sh
+++ web/backend-fastapi/entrypoint.sh
@@ -9,9 +9,6 @@ export AWS_SESSION_TOKEN="--"
 export AWS_DEFAULT_REGION=ca-central-1
 export AWS_REGION=ca-central-1
 
-
-
-
 # Check if the environment variables are loaded correctly
 if [ -z "$AWS_ACCESS_KEY_ID" ]; then
     echo "Required environment variable not set. Exiting..."
