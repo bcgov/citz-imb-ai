ca4a1906b4f7dddb2673869c319fd3690351b25e
Noor Chasib <noorchasib@gmail.com>
2024-06-06 11:05:06 -0700

    Update Context.tsx

--- web/frontend/src/context/Context.tsx
+++ web/frontend/src/context/Context.tsx
@@ -111,6 +111,8 @@ const ContextProvider: React.FC<ContextProviderProps> = ({ children }) => {
         });
         setIsAuthenticated(authenticated);
         if (authenticated) {
+          console.log("token", keycloak.token);
+          console.log("refresh token", keycloak.refreshToken);
           localStorage.setItem('keycloak-token', keycloak.token ?? '');
           localStorage.setItem(
             'keycloak-refresh-token',
