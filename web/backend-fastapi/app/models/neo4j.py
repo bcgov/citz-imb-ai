0a0f5721e550c9dd566fe14101b83b6897aa05a1
ANTSAND <antsand2002@gmail.com>
2024-06-06 14:08:02 -0700

    Update neo4j credentials

--- web/backend-fastapi/app/models/neo4j.py
+++ web/backend-fastapi/app/models/neo4j.py
@@ -1,4 +1,5 @@
 from langchain_community.graphs import Neo4jGraph
+import os
 
 def neo4j_vector_search(question, embeddings, kg):
     """Search for similar nodes using the Neo4j vector index"""
@@ -15,7 +16,10 @@ def neo4j_vector_search(question, embeddings, kg):
     return similar
 
 def neo4j():
-    NEO4J_URI = 'bolt://neo4j:7687'
+    NEO4J_URI = 'bolt://citz-imb-ai-neo4j-svc:7687'
+    NEO4J_USERNAME = os.getenv('NEO4J_USERNAME')
+    NEO4J_PASSWORD = os.getenv('NEO4J_PASSWORD')
+    NEO4J_DB = os.getenv('NEO4J_DB')
     kg = Neo4jGraph(
-        url=NEO4J_URI, username='neo4j', password='neo4j', database='neo4j')
+        url=NEO4J_URI, username=NEO4J_USERNAME, password=NEO4J_PASSWORD, database=NEO4J_DB)
     return kg
