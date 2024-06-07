b8215ceff3df80b7e69085b2266df52745a52c25
Noor Chasib <noorchasib@gmail.com>
2024-06-07 10:43:42 -0700

    change folder structure for apis

--- web/frontend/src/context/Context.tsx
+++ web/frontend/src/context/Context.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState, ReactNode, useEffect } from 'react';
-import runChat from '@/config/config';
+import runChat from '@/api/chat';
 import Keycloak from 'keycloak-js';
 
 interface ContextProps {
