4c082a747518c593ea3e27ad74a6067dd02626d5
ManishSihag <manish.sihag@yahoo.com>
2024-06-07 10:53:21 -0700

    add NEO4J_VECTOR_INDEX

--- web/backend-fastapi/app/models/neo4j.py
+++ web/backend-fastapi/app/models/neo4j.py
@@ -8,10 +8,11 @@ def neo4j_vector_search(question, embeddings, kg):
         CALL db.index.vector.queryNodes($index_name, $top_k, $question) yield node, score
         RETURN score, node.ActId,  node.RegId as Regulations, node.sectionId, node.sectionName, node.url,  node.text AS text
     """
-    similar = kg.query(vector_search_query, 
+    NEO4J_VECTOR_INDEX = os.getenv('NEO4J_VECTOR_INDEX')
+    similar = kg.query(vector_search_query,
                         params={
-                            'question': query_embedding, 
-                            'index_name':'Acts_Updatedchunks', 
+                            'question': query_embedding,
+                            'index_name': NEO4J_VECTOR_INDEX,
                             'top_k': 10})
     return similar
 
@@ -20,6 +21,5 @@ def neo4j():
     NEO4J_USERNAME = os.getenv('NEO4J_USERNAME')
     NEO4J_PASSWORD = os.getenv('NEO4J_PASSWORD')
     NEO4J_DB = os.getenv('NEO4J_DB')
-    kg = Neo4jGraph(
-        url=NEO4J_URI, username=NEO4J_USERNAME, password=NEO4J_PASSWORD, database=NEO4J_DB)
+    kg = Neo4jGraph(url=NEO4J_URI, username=NEO4J_USERNAME, password=NEO4J_PASSWORD, database=NEO4J_DB)
     return kg
