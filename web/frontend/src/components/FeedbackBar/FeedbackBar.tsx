2e47f260d3fbf12839503d138179853a629a6c01
Noor Chasib <noorchasib@gmail.com>
2024-06-10 16:19:07 -0700

    Update FeedbackBar.tsx

--- web/frontend/src/components/FeedbackBar/FeedbackBar.tsx
+++ web/frontend/src/components/FeedbackBar/FeedbackBar.tsx
@@ -2,13 +2,25 @@ import { assets } from '@/assets/icons/assets';
 import './FeedbackBar.scss';
 
 const FeedbackBar = () => {
+  const upvote = () => {
+    console.log('upvote, 1');
+  };
+
+  const downvote = () => {
+    console.log('downvote, 0');
+  };
+
   return (
     <div className="feedback-bar">
       <div className="feedback-buttons">
-        <button className="thumb-button" title="Good Response">
+        <button className="thumb-button" title="Good Response" onClick={upvote}>
           <img src={assets.thumbs_up} alt="Good Response" />
         </button>
-        <button className="thumb-button" title="Bad Response">
+        <button
+          className="thumb-button"
+          title="Bad Response"
+          onClick={downvote}
+        >
           <img src={assets.thumbs_down} alt="Bad Response" />
         </button>
       </div>
