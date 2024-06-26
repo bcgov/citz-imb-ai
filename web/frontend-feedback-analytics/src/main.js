ddcf960048732c521039811b001287b099e58a1a
ANTSAND <antsand2002@gmail.com>
2024-06-26 12:05:29 -0700

    update links

--- web/frontend-feedback-analytics/src/main.js
+++ web/frontend-feedback-analytics/src/main.js
@@ -1,16 +1,18 @@
-import './style.scss'
-import bclogo from '../assets/BC_logo.jpg'
-import { setupCounter } from '../counter.js'
+import "./style.scss";
+import bclogo from "../assets/BC_logo.jpg";
+import { setupCounter } from "../counter.js";
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
   <div>
     <a href="" target="_blank">
       <img src="${bclogo}" class="logo vanilla" alt="B.C logo" />
     </a>
     <h1>A.I Dashboard</h1>
+    <div style="display:flex; justify-content:space-evenly; align-items:center;">
     <button class="button" onclick="window.location.href = '/login.html'">Login</button>
-    <button class="button small" onclick="window.location.href = '/feedback.html'">Feedback</button>
+    <button class="button small" onclick="window.location.href = 'https://citz-imb-ai-frontend-route-b875cc-dev.apps.silver.devops.gov.bc.ca/'">B.C Gov A.I</button>
+    </div>
   </div>
-`
+`;
 
-setupCounter(document.querySelector('#counter'))
+setupCounter(document.querySelector("#counter"));
