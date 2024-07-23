e63a3ac1afab597543f8d6c46d7278d9f298e569
Noor Chasib <noorchasib@gmail.com>
2024-07-23 13:57:28 -0700

    Squashed commit of the following:
    commit 950dacb2ffd2b0e359692e424dc9b08a4c228e61
    Merge: 47fce97ed e3df3f4f0
    Author: ANTSAND <antsand2002@gmail.com>
    Date:   Tue Jul 23 13:13:48 2024 -0700
    
        Merge pull request #157 from bcgov/install_requirements_dag
    
        add full pat of requirements.txt
    
    commit e3df3f4f06078d6c0b8149ca2d14a48a318ac697
    Author: ANTSAND <antsand2002@gmail.com>
    Date:   Tue Jul 23 13:11:10 2024 -0700
    
        update requirements in dockerfile
    
    commit 47fce97ed9aa6df31c0085c7996275355f88aae3
    Merge: 63245738c e9dcdb9aa
    Author: ANTSAND <antsand2002@gmail.com>
    Date:   Tue Jul 23 12:51:22 2024 -0700
    
        Merge pull request #157 from bcgov/install_requirements_dag
    
        add full pat of requirements.txt
    
    commit e9dcdb9aaef08a2d6c4f9d8679fec01fb069b171
    Author: ANTSAND <antsand2002@gmail.com>
    Date:   Tue Jul 23 12:49:44 2024 -0700
    
        add full pat of requirements.txt
    
    commit 63245738cd5761c9d42edaf6057e4b50b62d34f6
    Merge: 959a20cf8 4c12a7f21
    Author: ANTSAND <antsand2002@gmail.com>
    Date:   Tue Jul 23 12:41:39 2024 -0700
    
        Merge pull request #156 from bcgov/install_requirements_dag
    
        Install requirements dag
    
    commit 4c12a7f2196887509afe1c4d46f071998a92d859
    Merge: 462f4fae3 959a20cf8
    Author: ANTSAND <antsand2002@gmail.com>
    Date:   Tue Jul 23 12:39:45 2024 -0700
    
        Merge branch 'main' of https://github.com/bcgov/citz-imb-ai into install_requirements_dag
    
    commit 959a20cf83f9fe2cd160c740af9a2010fcd263e7
    Merge: c00309edf f28f7b579
    Author: ANTSAND <antsand2002@gmail.com>
    Date:   Tue Jul 23 12:39:18 2024 -0700
    
        Merge pull request #155 from bcgov/hpc_server_threading
    
        modularize the code and have a common act/reg function
    
    commit 462f4fae337dafa563d3bf347bc0d5e0aa6cc8af
    Author: ANTSAND <antsand2002@gmail.com>
    Date:   Tue Jul 23 12:20:25 2024 -0700
    
        Add requirement dag
    
    commit f28f7b5796733ae95f28f35d6661716912919b48
    Author: ANTSAND <antsand2002@gmail.com>
    Date:   Tue Jul 23 11:23:42 2024 -0700
    
        Add comments for the AVX-512 tolower function
    
    commit 05e2f77e1e75a1a9bddb0388472203e5e31d429d
    Author: ANTSAND <antsand2002@gmail.com>
    Date:   Tue Jul 23 11:12:56 2024 -0700
    
        modularize the code and have a common act/reg function
    
    commit c00309edfec46e9b5b728b059ecfe870dc62bee1
    Merge: c057abba8 52a7b1b58
    Author: ANTSAND <antsand2002@gmail.com>
    Date:   Tue Jul 23 10:48:28 2024 -0700
    
        Merge pull request #154 from bcgov/airflow_gaphicdata_ticket
    
        Update requirement.txt file location
    
    commit 52a7b1b58d9a3eef9fc066466e7a7ce15c0aa2de
    Author: ANTSAND <antsand2002@gmail.com>
    Date:   Tue Jul 23 10:46:21 2024 -0700
    
        Update requirement.txt file location
    
    commit c057abba867593c353e93886b6ad434d9a7979ae
    Merge: 1693b6460 c92f9291c
    Author: ANTSAND <antsand2002@gmail.com>
    Date:   Tue Jul 23 08:34:46 2024 -0700
    
        Merge pull request #153 from bcgov/airflow_gaphicdata_ticket
    
        Add airflow tags
    
    commit c92f9291c90c45dd6490911255d9c2547748e5ab
    Author: ANTSAND <antsand2002@gmail.com>
    Date:   Tue Jul 23 08:30:38 2024 -0700
    
        Add airflow tags
    
    commit 1693b64609401f59792eea888e985e93286ed3f5
    Merge: be25cd9f4 3cd6c6778
    Author: ANTSAND <antsand2002@gmail.com>
    Date:   Mon Jul 22 15:50:18 2024 -0700
    
        Merge pull request #152 from bcgov/airflow_gaphicdata_ticket
    
        Airflow gaphicdata ticket
    
    commit 3cd6c677843d4c0afc3191802e719c353677736b
    Author: ANTSAND <antsand2002@gmail.com>
    Date:   Mon Jul 22 15:36:56 2024 -0700
    
        Added airflow graphics data
    
    commit 1f47b5827f99250c595d84e111d93680663d8cce
    Author: ANTSAND <antsand2002@gmail.com>
    Date:   Fri Jul 19 12:51:31 2024 -0700
    
        update acts and regs to acts_regs

--- mlops/orchestration/airflow/dags/retrieve_data_dag.py
+++ mlops/orchestration/airflow/dags/retrieve_data_dag.py
@@ -1,5 +1,107 @@
-
 from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+import boto3
+import os
+from dotenv import load_dotenv
+
+load_dotenv("/vault/secrets/zuba-secret-dev")
+# Set environment variables
+S3_ACCESS_KEY = os.getenv('S3_ACCESS_KEY')
+S3_SECRET_ACCESS_KEY = os.getenv('S3_SECRET_ACCESS_KEY')
+S3_ENDPOINT_URL = os.getenv('S3_ENDPOINT_URL')
+
+print(S3_ACCESS_KEY)
+print(S3_ENDPOINT_URL)
+print(S3_SECRET_ACCESS_KEY)
+
+linode_obj_config = {
+    "aws_access_key_id": S3_ACCESS_KEY,
+    "aws_secret_access_key": S3_SECRET_ACCESS_KEY,
+    "endpoint_url": S3_ENDPOINT_URL,
+}
+bucket_name = "IMBAIPilot"
+
+def download_data(bucket, path, bucket_name):
+    prefix = bucket
+    client = boto3.client("s3", **linode_obj_config)
+    print(client)
+    BASE_PATH = path
+    if not os.path.exists(BASE_PATH): os.makedirs(BASE_PATH, mode=0o777)
+    
+    continuation_token = None
+
+    while True:
+        if continuation_token:
+            response = client.list_objects_v2(Bucket=bucket_name, Prefix=prefix, ContinuationToken=continuation_token)
+        else:
+            response = client.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+            print(response)
+        
+        if 'Contents' in response:
+            for obj in response['Contents']:
+                newpath = os.path.join(BASE_PATH, obj['Key'].split('/')[-1])
+                if newpath != path:
+                    print(newpath)
+                    client.download_file(bucket_name, obj['Key'], newpath)
+
+        # Check if more pages are available
+        if response.get('IsTruncated'):  # If the response is truncated, there are more pages to retrieve
+            continuation_token = response.get('NextContinuationToken')
+        else:
+            break
+
+def download_bclaws_acts():
+    download_data("bclaws/data/xml/Cleaned/Acts", "XML_Acts/", bucket_name)
+
+def download_bclaws_regulations():
+    download_data("bclaws/data/xml/Cleaned/Regulations", "XML_Regulations/", bucket_name)
+
+def download_bclaws_glossary():
+    download_data("bclaws/glossary", "JSON_glossary/", bucket_name)
+
+def download_ticket_graphicdata():
+    download_data("bclaws/data_v2/OCR/", "JSON_graphicdata/", bucket_name)
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+}
+
+with DAG(
+    'retrieve_data_from_s3_dag',
+    default_args=default_args,
+    description='Retrieve B.C. laws data from S3',
+    schedule_interval='@daily',
+    start_date=days_ago(1),
+    catchup=False,
+    tags=['bclaws', 's3'],
+) as dag:
+
+    task_download_acts = PythonOperator(
+        task_id='download_bclaws_acts',
+        python_callable=download_bclaws_acts,
+    )
+
+    task_download_regulations = PythonOperator(
+        task_id='download_bclaws_regulations',
+        python_callable=download_bclaws_regulations,
+    )
+    
+    task_download_glossary = PythonOperator(
+        task_id='download_bclaws_glossary',
+        python_callable=download_bclaws_glossary,
+    )
+
+    task_download_graphicdata = PythonOperator(
+        task_id='download_ticket_graphicdata',
+        python_callable=download_ticket_graphicdata,
+    )
+
+    task_download_acts >> task_download_regulations >> task_download_glossary >> task_download_graphicdatafrom airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
 import boto3
