ab54886bbf82f6de6dbaaedfbc43a6ed38420b90
Noor Chasib <noorchasib@gmail.com>
2024-08-20 11:24:42 -0700

    Update analytics.types.ts

--- web/frontend/src/types/types/analytics.types.ts
+++ web/frontend/src/types/types/analytics.types.ts
@@ -1,5 +1,5 @@
 export interface SourceInteraction {
-  key: number;
+  sourceKey: number;
   response: string;
   clicks: number;
   lastClickTimestamp: string;
@@ -10,6 +10,7 @@ export interface LLMResponseInteraction {
   hoverDuration: number;
   clicks: number;
   lastClickTimestamp: string;
+  chatIndex: number;
 }
 
 export interface ChatInteraction {
