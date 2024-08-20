bda8732d59c6f69d26d34b0685350bc92903d182
Noor Chasib <noorchasib@gmail.com>
2024-08-19 22:17:00 -0700

    Update analytics.ts

--- web/frontend/src/api/analytics.ts
+++ web/frontend/src/api/analytics.ts
@@ -7,23 +7,17 @@ export const sendAnalyticsDataToBackend = async (
   data: AnalyticsData,
   useKeepalive = false,
 ): Promise<void> => {
-  try {
-    const response = await fetch(url, {
-      keepalive: useKeepalive,
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${localStorage.getItem('keycloak-token')}`,
-      },
-      body: JSON.stringify(data),
-    });
+  const response = await fetch(url, {
+    keepalive: useKeepalive,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${localStorage.getItem('keycloak-token')}`,
+    },
+    body: JSON.stringify(data),
+  });
 
-    if (!response.ok) {
-      throw new Error('Failed to send analytics data to the backend');
-    }
-
-    console.log('Analytics data successfully sent to the backend');
-  } catch (error) {
-    console.error('Error sending analytics data:', error);
+  if (!response.ok) {
+    throw new Error('Failed to send analytics data to the backend');
   }
 };
