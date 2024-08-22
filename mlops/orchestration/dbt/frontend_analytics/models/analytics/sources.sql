f69feb8cb296d140a91f4366a0a3e15f1929726b
Noor Chasib <noorchasib@gmail.com>
2024-08-22 10:24:29 -0700

    fix raw data source and quoutes

--- mlops/orchestration/dbt/frontend_analytics/models/analytics/sources.sql
+++ mlops/orchestration/dbt/frontend_analytics/models/analytics/sources.sql
@@ -1,13 +1,13 @@
 {{ config(materialized='table') }}
 
 SELECT
-    {{ dbt_utils.generate_surrogate_key(['data->>'sessionId'', 'chat->>'llmResponseId'', 'source->>'sourceKey'']) }} AS source_id,
-    {{ dbt_utils.generate_surrogate_key(['data->>'sessionId'', 'chat->>'llmResponseId'']) }} AS chat_id,
+    {{ dbt_utils.generate_surrogate_key(["data->>'sessionId'", "chat->>'llmResponseId'", "source->>'sourceKey'"]) }} AS source_id,
+    {{ dbt_utils.generate_surrogate_key(["data->>'sessionId'", "chat->>'llmResponseId'"]) }} AS chat_id,
     (source->>'chatIndex')::int AS chat_index,
     (source->>'sourceKey')::int AS source_key,
     source->>'response' AS response,
     (source->>'clicks')::int AS clicks,
     (source->>'lastClickTimestamp')::timestamp AS last_click_timestamp
-FROM {{ source('frontend_analytics', 'raw_data') }},
+FROM {{ source('frontend_analytics', 'raw_frontend_analytics') }},
     jsonb_array_elements(data->'chats') AS chat,
     jsonb_array_elements(chat->'sources') AS source
