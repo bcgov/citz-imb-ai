6bd3f71c69b54d6ea9e79612f9331e8d8002b837
Noor Chasib <noorchasib@gmail.com>
2024-08-22 10:00:05 -0700

    create necessary tables and sql transformations for normalized data

---
+++ mlops/orchestration/dbt/frontend_analytics/models/analytics/chats.sql
@@ -0,0 +1,10 @@
+{{ config(materialized='table') }}
+
+SELECT
+    {{ dbt_utils.generate_surrogate_key(['data->>'sessionId'', 'chat->>'llmResponseId'']) }} AS chat_id,
+    data->>'sessionId' AS session_id,
+    chat->>'llmResponseId' AS llm_response_id,
+    chat->>'recording_id' AS recording_id,
+    (chat->>'timestamp')::timestamp AS timestamp
+FROM {{ source('frontend_analytics', 'raw_data') }},
+    jsonb_array_elements(data->'chats') AS chat
