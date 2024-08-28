5d40c7b80a5a70dae5ca247341db5d06aab6ae67
Noor Chasib <noorchasib@gmail.com>
2024-08-28 14:44:13 -0700

    remove extra model

--- mlops/orchestration/dbt/analytics/models/frontend/raw_analytics_model.sql
+++
@@ -1,8 +0,0 @@
-{{ config(materialized='table') }}
-
-SELECT
-    data->>'sessionTimestamp' AS session_timestamp,
-    data->>'sessionId' AS session_id,
-    data->>'userId' AS user_id,
-    data->'chats' AS chats
-FROM {{ source('frontend', 'raw_frontend_analytics') }}
