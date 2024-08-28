266735333a0e13113bcc00c667858a45ebdef7e5
Noor Chasib <noorchasib@gmail.com>
2024-08-28 14:44:49 -0700

    add columns to chat table

--- mlops/orchestration/dbt/analytics/models/frontend/chats.sql
+++ mlops/orchestration/dbt/analytics/models/frontend/chats.sql
@@ -5,6 +5,10 @@ SELECT
     data->>'sessionId' AS session_id,
     chat->>'llmResponseId' AS llm_response_id,
     chat->>'recording_id' AS recording_id,
-    (chat->>'timestamp')::timestamp AS timestamp
+    (chat->>'timestamp')::timestamp AS timestamp,
+    (chat->'llmResponseInteraction'->>'chatIndex')::int AS chat_index,
+    (chat->'llmResponseInteraction'->>'clicks')::int AS llm_response_clicks,
+    (chat->'llmResponseInteraction'->>'hoverDuration')::float AS llm_response_hover_duration,
+    NULLIF(chat->'llmResponseInteraction'->>'lastClickTimestamp', '')::timestamp AS llm_response_last_click_timestamp
 FROM {{ source('frontend', 'raw_frontend_analytics') }},
     jsonb_array_elements(data->'chats') AS chat
