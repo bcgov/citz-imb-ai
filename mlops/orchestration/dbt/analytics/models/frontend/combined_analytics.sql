f63d2ed4752476f2546a2f4618e5c2c821ae9ce3
Noor Chasib <noorchasib@gmail.com>
2024-08-29 14:00:51 -0700

    create combined view for analytics

---
+++ mlops/orchestration/dbt/analytics/models/frontend/combined_analytics.sql
@@ -0,0 +1,15 @@
+{{ config(materialized='view') }}
+
+SELECT
+    fa.*,
+    tn.input,
+    tn.app_id,
+    tn.LLM_output,
+    tn.json_record_id,
+    tn.start_time,
+    tn.end_time,
+    tn.latency_in_seconds,
+    {{ dbt_utils.star(from=ref('trulens_normalize'), except=['record_id', 'input', 'app_id', 'LLM_output', 'json_record_id', 'start_time', 'end_time', 'latency_in_seconds']) }}
+FROM {{ ref('normalized_analytics') }} fa
+LEFT JOIN {{ ref('trulens_normalize') }} tn
+    ON fa.record_id = tn.record_id
