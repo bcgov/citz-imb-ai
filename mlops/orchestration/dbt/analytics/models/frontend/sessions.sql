2a6b1f2486d0275e4404f5959e91ba515ba2926e
Noor Chasib <noorchasib@gmail.com>
2024-08-28 17:53:31 -0700

    remove extra tables to consolidate

--- mlops/orchestration/dbt/analytics/models/frontend/sessions.sql
+++
@@ -1,7 +0,0 @@
-{{ config(materialized='table') }}
-
-SELECT DISTINCT
-    data->>'sessionId' AS session_id,
-    (data->>'sessionTimestamp')::timestamp AS session_timestamp,
-    data->>'userId' AS user_id
-FROM {{ source('frontend', 'raw_frontend_analytics') }}
