b85fabfb3d1f937331824a30332a5e25fe70976a
dbarkowsky <dylanbarkowsky@gmail.com>
2024-10-30 15:35:08 -0700

    add cross-encoding to full rag

--- web/backend-fastapi/app/models/rag.py
+++ web/backend-fastapi/app/models/rag.py
@@ -2,14 +2,18 @@ from trulens_eval.tru_custom_app import instrument
 from app.models import neo4j, trulens, rag, bedrock
 import json
 from typing import List
+from sentence_transformers import CrossEncoder
+
 
 def retrieval(query_str, embeddings, kg):
     return neo4j.neo4j_vector_search(query_str, embeddings, kg)
+
 
 class ChatHistory:
     def __init__(self, prompt: str, response: str):
         self.prompt = prompt
         self.response = response
+
 
 class get_top_k:
     @instrument
@@ -17,12 +21,13 @@ class get_top_k:
         """
         Retrieve relevant text from vector store.
         """
-        return retrieval(query, embeddings, kg)    
+        return retrieval(query, embeddings, kg)
 
     @instrument
     def query(self, query: str, chat_history: List[ChatHistory], embeddings, kg) -> str:
         context_str = self.retrieve(query, embeddings, kg)
         return context_str
+
 
 class get_full_rag:
     @instrument
@@ -32,13 +37,17 @@ class get_full_rag:
         """
         print("retrieval processing")
         return retrieval(query, embeddings, kg)
-    
+
     @instrument
-    def create_prompt(self, query: str, context_str: str, chat_history: List[ChatHistory]) -> str:
+    def create_prompt(
+        self, query: str, context_str: str, chat_history: List[ChatHistory]
+    ) -> str:
         """
         Generate a response using the given context and chat history.
         """
-        chat_history_str = "\n".join([f"Human: {ch.prompt}\nAI: {ch.response}" for ch in chat_history])
+        chat_history_str = "\n".join(
+            [f"Human: {ch.prompt}\nAI: {ch.response}" for ch in chat_history]
+        )
         print("chat_history_str: ", chat_history_str)
         messages = f"""
             You are a helpful and knowledgeable assistant. Use the following information to answer the user's question accurately and concisely. Do not provide information that is not supported by the given context or chat history.
@@ -65,29 +74,47 @@ class get_full_rag:
     def get_response(self, query: str) -> str:
         bedrock_response = bedrock.get_response(query)
         return bedrock_response
-    
+
     def formatoutput(self, topk, lm_output):
         prettier = {}
-        prettier['llm'] = lm_output
-        prettier['topk'] = []
+        prettier["llm"] = lm_output
+        prettier["topk"] = []
         for k in topk:
             k_obj = {}
-            k_obj['score'] = k['score']
-            k_obj['ActId'] = k['node.ActId']
-            k_obj['Regulations'] = k['Regulations']
-            k_obj['sectionId'] = k['node.sectionId']
-            k_obj['sectionName'] = k['node.sectionName']
-            k_obj['url'] = k['node.url']
-            k_obj['text'] = k['text']
-            if 'references' in k:
-                k_obj['references'] = k['references']
-            prettier['topk'].append(k_obj)
+            k_obj["score"] = k["score"]
+            k_obj["ActId"] = k["node.ActId"]
+            k_obj["Regulations"] = k["Regulations"]
+            k_obj["sectionId"] = k["node.sectionId"]
+            k_obj["sectionName"] = k["node.sectionName"]
+            k_obj["url"] = k["node.url"]
+            k_obj["text"] = k["text"]
+            if "references" in k:
+                k_obj["references"] = k["references"]
+            prettier["topk"].append(k_obj)
         return prettier
+
+    @instrument
+    def re_rank_reference(self, topk, compared_text):
+        # Map topk to [query, text] pairs list
+        pairs = [[compared_text, doc["text"]] for doc in topk]
+        # Produces list of float values. Higher is more closely related. Should be parallel with pairs list.
+        model = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
+        scores = model.predict(pairs)
+        # Apply these scores to the original objects
+        for index, _ in enumerate(topk):
+            topk[index]["cross_score"] = scores[index]
+        # Sort by these new scores
+        topk.sort(key=lambda x: x["cross_score"], reverse=True)
+        return topk
 
     @instrument
     def query(self, query: str, chat_history: List[ChatHistory], embeddings, kg) -> str:
         context_str = self.retrieve(query, embeddings, kg)
+        # Rerank once to order for LLM
+        context_str = self.re_rank_reference(context_str, query)
         create_prompt = self.create_prompt(query, context_str, chat_history)
         bedrock_response = self.get_response(create_prompt)
+        # Rerank again to sort references by relevance to response
+        context_str = self.re_rank_reference(context_str, bedrock_response)
         pretty_output = self.formatoutput(context_str, bedrock_response)
         return json.dumps(pretty_output)
