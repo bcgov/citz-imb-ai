7328d72fec8606b8e87713d1f5e5935b072e8c41
Noor Chasib <noorchasib@gmail.com>
2024-11-15 15:15:25 -0800

    fix linting errors

--- web/frontend/src/utils/authUtil.ts
+++ web/frontend/src/utils/authUtil.ts
@@ -6,11 +6,13 @@ import { Context } from '@/context/Context';
 export const isAuthenticated = (): boolean => {
   const context = useContext(Context);
   const isAuthenticated = context ? context.isAuthenticated : false;
-  return isAuthenticated;
+  
+return isAuthenticated;
 };
 
 // Get the user ID from local storage
 export const getUserId = (): string => {
   const userInfo = localStorage.getItem('keycloak-user-id') || '';
-  return userInfo;
+  
+return userInfo;
 };
