0ffbce4991bc96922536aa4dcf4c0a7b666abb00
Noor Chasib <noorchasib@gmail.com>
2024-11-15 17:28:38 -0800

    remove unnecessary try catch block

--- web/frontend/src/api/chat.ts
+++ web/frontend/src/api/chat.ts
@@ -5,37 +5,33 @@ const runChat = async (
   _prompt: string,
   chatHistory: ChatHistory[],
 ): Promise<{ response: ApiResponse; recordingHash: string }> => {
-  try {
-    // Send a POST request to the chat endpoint
-    const response = await fetch('/api/chat/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${localStorage.getItem('keycloak-token')}`,
-      },
-      body: JSON.stringify({
-        prompt: _prompt,
-        chatHistory: chatHistory,
-      }),
-    });
+  // Send a POST request to the chat endpoint
+  const response = await fetch('/api/chat/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${localStorage.getItem('keycloak-token')}`,
+    },
+    body: JSON.stringify({
+      prompt: _prompt,
+      chatHistory: chatHistory,
+    }),
+  });
 
-    // Throw an error if the response is not successful
-    if (!response.ok) {
-      throw new Error(`${response.status}`);
-    }
-
-    // Parse the response data
-    const data = await response.json();
-    const responses: ApiResponse = JSON.parse(data.responses);
-
-    // Return the API response and recording hash
-    return {
-      response: responses,
-      recordingHash: data.recording,
-    };
-  } catch (error) {
-    throw error;
+  // Throw an error if the response is not successful
+  if (!response.ok) {
+    throw new Error(`${response.status}`);
   }
+
+  // Parse the response data
+  const data = await response.json();
+  const responses: ApiResponse = JSON.parse(data.responses);
+
+  // Return the API response and recording hash
+  return {
+    response: responses,
+    recordingHash: data.recording,
+  };
 };
 
 export default runChat;
