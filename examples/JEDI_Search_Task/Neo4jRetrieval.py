5bf2a12cc00791af80b932bd5750d9f3105e7b55
dbarkowsky <dylanbarkowsky@gmail.com>
2025-05-27 16:17:15 -0700

    getting nodes for jedi task

---
+++ examples/JEDI_Search_Task/Neo4jRetrieval.py
@@ -0,0 +1,41 @@
+class Neo4jRetrieval:
+    def __init__(self, uri, user, password):
+        from neo4j import GraphDatabase
+
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+
+    def close(self):
+        self.driver.close()
+
+    def run_query(self, query):
+        with self.driver.session() as session:
+            result = session.run(query)
+            return [record for record in result]
+
+    def search(self, search_term):
+        query = f"MATCH (n:UpdatedChunk) WHERE n.text CONTAINS '{search_term}' RETURN n"
+        return self.run_query(query)
+
+    def search_many(self, terms: list[str]) -> list[dict]:
+        query = f"""
+        UNWIND $terms AS term
+        MATCH (n:UpdatedChunk)
+        WHERE n.text CONTAINS term
+        RETURN DISTINCT {{text: n.text, ActId: n.ActId, elementId: elementId(n), RegId: n.RegId, sectionName: n.sectionName }} AS n
+        """
+        with self.driver.session() as session:
+            result = session.run(query, terms=terms)
+            return [record["n"] for record in result]
+
+
+# Intended for local development and testing
+# NEO4J_URI = "bolt://" + "localhost:7687"  # os.getenv("NEO4J_HOST") + ":7687"
+# NEO4J_USERNAME = "admin"  # os.getenv("NEO4J_USER")
+# NEO4J_PASSWORD = "admin"  # os.getenv("NEO4J_PASSWORD")
+
+# neo4j = Neo4jRetrieval(NEO4J_URI, NEO4J_USERNAME, NEO4J_PASSWORD)
+# # Example usage
+# results = neo4j.search_many(["cat", "dog"])
+# print(len(results))
+# # Close the connection when done
+# neo4j.close()
